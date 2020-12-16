e9f8fc7caf101f827a0442f01487bbdb76791e6c
Niv Suroghon <nsuroghon@gmail.com>
2020-12-15 21:58:42 -0500

    reading db.json file

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,6 @@
 // dependencies
 const express = require('express');
+const fs = require('fs');
 const path = require ('path');
 
 // app
@@ -11,28 +12,33 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// notes data
+// notes array
 let notes = [];
 
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+module.exports = (app) => {
+// html routes 
+  app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+  app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
 // * `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
-app.get('/api/notes', (req, res) => res.json(json));
+  let data = JSON.parse(fs.readFileSync('./db/db.json', "utf8"))
+  app.get('/api/notes', (req, res) => res.json(data));
 
 // * `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, 
 // and then return the new note to the client. You'll need to find a way to give each note a unique id 
 // when it's saved (look into `npm` packages that could do this for you).
 
-app.post('/api/notes', (req, res) => {
-  const addNote = req.body; 
-  notes.push(addNote);
-  res.json(addNote);
-});
+  app.post('/api/notes', (req, res) => {
+    const addNote = req.body; 
+    notes.push(addNote);
+    res.json(addNote);
+  });
+}
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, () => {
     // Log (server-side) when our server has started
-    console.log(`Server listening on: http://localhost:${PORT}`);
+    console.log(`App listening on: http://localhost:${PORT}`);
   });
+
