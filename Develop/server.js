fe697723d8aa3ca018160d821bebf38b7d2ddc63
Niv Suroghon <nsuroghon@gmail.com>
2020-12-15 22:00:17 -0500

    return data in json format

--- Develop/server.js
+++ Develop/server.js
@@ -23,7 +23,9 @@ module.exports = (app) => {
 
 // * `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
   let data = JSON.parse(fs.readFileSync('./db/db.json', "utf8"))
-  app.get('/api/notes', (req, res) => res.json(data));
+  app.get('/api/notes', (req, res) => {
+    return res.json(data)
+  });
 
 // * `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, 
 // and then return the new note to the client. You'll need to find a way to give each note a unique id 
